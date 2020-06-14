7180a58c56e867d06c6d19ef56d2a95430e18e44
Amit Banerjee <parkouramit@gmail.com>
2020-06-14 20:01:48 +0530

    UI changes in settings activity and pre-filled data in the form

--- app/src/main/java/com/example/learn/SettingsActivity.kt
+++ app/src/main/java/com/example/learn/SettingsActivity.kt
@@ -2,11 +2,19 @@ package com.example.learn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.learn.Utils.AuthenticationToken
+import com.example.learn.Utils.LoadImage
+import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
+    lateinit var authenticationToken:AuthenticationToken
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        authenticationToken=AuthenticationToken(this)
+        val displayImageUrl=authenticationToken.getUserDisplayPictureUrl()
+        LoadImage(profileImage,displayImageUrl)
+        profileName.text=authenticationToken.getUserName()
     }
 }
